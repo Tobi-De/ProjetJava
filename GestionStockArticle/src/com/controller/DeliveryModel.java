5f52f12791ab2f79aecb9ce3e1d7682fa8caa497
Tobi <degnonfrancis@gmai.com>
2019-07-02 09:44:46 +0100

    Modification du diagramme de classe

--- GestionStockArticle/src/com/controller/DeliveryModel.java
+++ GestionStockArticle/src/com/controller/DeliveryModel.java
@@ -1,4 +1,12 @@
 package com.controller;
 
 public abstract   class DeliveryModel {
+    private int stockMaximun;
+    private int stockMinimum;
+    private int quantityToOrder;
+    private String description;
+
+    public isSupplyNecessary(){
+        return this.stockMinimum 
+    }
 }
