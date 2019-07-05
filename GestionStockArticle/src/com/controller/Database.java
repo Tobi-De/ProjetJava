3fd2d4efd98d7d3f75adb18d7316ca0d701d7e81
Tobi <degnonfrancis@gmai.com>
2019-07-05 12:51:09 +0100

    modfi++

---
+++ GestionStockArticle/src/com/controller/Database.java
@@ -0,0 +1,38 @@
+package com.controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+///import java.sql.Statement;
+public class Database {
+
+    public static  void connect(){
+        Connection con = null;
+        String dbpath="gestion.db";
+        try{
+            Class.forName("org.sqlite.JDBC");
+            String path="jdbc:sqlite:"+dbpath;
+            con = DriverManager.getConnection(path);
+            System.out.println("Connection to sqlite has been established ");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally{
+            try{
+                if(con!=null)
+                {
+                    System.out.println("Connexion fermée");
+                    con.close();
+                }
+            }
+            catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        connect();
+    }
+}
